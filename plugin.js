dd7ec45c4a8acf76cbc66aa72af58a1314701d53
Goodnessuc <ukejegoodness599@gmail.com>
2024-06-18 08:39:06 +0100

    [fix]: fixed README jargons

--- plugin.js
+++ plugin.js
@@ -1,15 +1,8 @@
 const fastify = require('fastify')();
+const cors_plugin = require('@fastify/cors')
 
 // Example: Registering a CORS plugin
-fastify.register(require('@fastify/cors'), {
+
+fastify.register(cors_plugin, {
     origin: '*'
-});
-
-fastify.get('/', async (request, reply) => {
-    return { hello: 'world' };
-});
-
-fastify.listen({ port: 3000, host: '0.0.0.0' }, err => {
-    if (err) throw err;
-    console.log('Server listening on http://localhost:3000');
 });
